89848363daa3cf5e1e07a91122475e0326ccaf84
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-05-28 19:26:13 -0300

    Inserindo validação customizadas,validação de CPF, validação de senha

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgBrazilValidators } from 'ng-brazil';
 import { Usuario } from './models/usuario';
+import { utilsBr } from 'js-brasil';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,23 +14,28 @@ export class CadastroComponent implements OnInit {
   cadastroForm: FormGroup;
   usuario: Usuario;
   formResult: string = '';
+  MASKS = utilsBr.MASKS;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15]), CustomValidators.equalTo(senha)]);
+
     this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required],
-      cpf: ['', Validators.required],
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(150)]],
+      cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
       email: ['',[Validators.required, Validators.email]],
-      senha: ['', Validators.required],
-      senhaConfirmacao: ['', Validators.required],
+      senha: senha,
+      senhaConfirmacao: senhaConfirm,
     });
   }
 
   adicionarUsuario(){
     if(this.cadastroForm.dirty && this.cadastroForm.valid){
       this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
-      this.formResult = JSON.stringify(this.cadastroForm.value)
+      this.formResult = JSON.stringify(this.cadastroForm.value);
     }
     else{
       this.formResult = "Não submeteu!!"
